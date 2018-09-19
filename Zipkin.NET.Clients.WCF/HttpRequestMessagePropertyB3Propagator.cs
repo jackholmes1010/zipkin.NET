6bc3af0104a78edc4e1d1c04208e5c6b154a54f9
jack <jackholmes5194@gmail.com>
2018-09-19 19:31:38 +1200

    fix wcf client

--- Zipkin.NET.Clients.WCF/HttpRequestMessagePropertyB3Propagator.cs
+++ Zipkin.NET.Clients.WCF/HttpRequestMessagePropertyB3Propagator.cs
@@ -1,35 +1,20 @@
-﻿//using System.ServiceModel.Channels;
-//using Zipkin.NET.Instrumentation;
-//using Zipkin.NET.Instrumentation.Constants;
-//using Zipkin.NET.Instrumentation.Propagation;
+﻿using System.ServiceModel.Channels;
+using Zipkin.NET.Constants;
+using Zipkin.NET.Models;
+using Zipkin.NET.Propagation;
 
-//namespace Zipkin.NET.Clients.WCF
-//{
-//    /// <summary>
-//    /// Injects trace context into an <see cref="HttpRequestMessageProperty"/>.
-//    /// </summary>
-//    public class HttpRequestMessagePropertyB3Propagator : IPropagator<HttpRequestMessageProperty>
-//    {
-//        /// <summary>
-//        /// Adds X-B3 header values to an outgoing HTTP request.
-//        /// </summary>
-//        /// <param name="httpRequest">
-//        /// The request to add headers to.
-//        /// </param>
-//        /// <param name="context">
-//        /// The <see cref="TraceContext"/> which contains trace ID context for the current trace.
-//        /// </param>
-//        /// <returns>
-//        /// The HTTP request with trace headers.
-//        /// </returns>
-//        public HttpRequestMessageProperty Inject(HttpRequestMessageProperty httpRequest, TraceContext context)
-//        {
-//            httpRequest.Headers.Add(B3HeaderConstants.TraceId, context.TraceId);
-//            httpRequest.Headers.Add(B3HeaderConstants.SpanId, context.SpanId);
-//            httpRequest.Headers.Add(B3HeaderConstants.ParentSpanId, context.ParentSpanId);
-//            httpRequest.Headers.Add(B3HeaderConstants.Sampled, context.Sampled == true ? "1" : "0");
-//            httpRequest.Headers.Add(B3HeaderConstants.Flags, context.Debug == true ? "1" : "0");
-//            return httpRequest;
-//        }
-//    }
-//}
+namespace Zipkin.NET.Clients.WCF
+{
+    public class HttpRequestMessagePropertyB3Propagator : IPropagator<HttpRequestMessageProperty>
+    {
+        public HttpRequestMessageProperty Inject(HttpRequestMessageProperty inject, Span span)
+        {
+            inject.Headers.Add(B3HeaderConstants.TraceId, span.TraceId);
+            inject.Headers.Add(B3HeaderConstants.SpanId, span.Id);
+            inject.Headers.Add(B3HeaderConstants.ParentSpanId, span.ParentId);
+            //inject.Headers.Add(B3HeaderConstants.Sampled, span.Sampled == true ? "1" : "0");
+            inject.Headers.Add(B3HeaderConstants.Flags, span.Debug ? "1" : "0");
+            return inject;
+        }
+    }
+}
--- Zipkin.NET.Clients.WCF/ZipkinMessageInspector.cs
+++ Zipkin.NET.Clients.WCF/ZipkinMessageInspector.cs
@@ -1,90 +1,76 @@
-﻿//using System.ServiceModel;
-//using System.ServiceModel.Channels;
-//using System.ServiceModel.Dispatcher;
-//using Zipkin.NET.Instrumentation;
-//using Zipkin.NET.Instrumentation.Models;
-//using Zipkin.NET.Instrumentation.Propagation;
-//using Zipkin.NET.Instrumentation.Reporting;
-//using Zipkin.NET.Instrumentation.Sampling;
-//using Zipkin.NET.Instrumentation.Traces;
+﻿using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
+using Zipkin.NET.Models;
+using Zipkin.NET.Propagation;
+using Zipkin.NET.Reporters;
 
-//namespace Zipkin.NET.Clients.WCF
-//{
-//    /// <summary>
-//    /// Message inspector responsible for adding Zipkin X-B3 HTTP headers to outgoing WCF requests.
-//    /// </summary>
-//    /// <inheritdoc />
-//    public class ZipkinMessageInspector : IClientMessageInspector
-//    {
-//        private readonly string _applicationName;
-//        private readonly IReporter _reporter;
-//        private readonly ISampler _sampler;
-//        private readonly ITraceContextAccessor _traceContextAccessor;
-//        private readonly IPropagator<HttpRequestMessageProperty> _propagator;
+namespace Zipkin.NET.Clients.WCF
+{
+    public class ZipkinMessageInspector : IClientMessageInspector
+    {
+        private readonly string _applicationName;
+        private readonly IReporter _reporter;
+        private readonly ITraceAccessor _traceAccessor;
+        private readonly IPropagator<HttpRequestMessageProperty> _propagator;
 
-//        private ClientTrace _trace;
+        private SpanBuilder _spanBuilder;
 
-//        public ZipkinMessageInspector(
-//            string applicationName,
-//            IReporter reporter,
-//            ISampler sampler,
-//            ITraceContextAccessor traceContextAccessor,
-//            IPropagator<HttpRequestMessageProperty> propagator)
-//        {
-//            _applicationName = applicationName;
-//            _reporter = reporter;
-//            _sampler = sampler;
-//            _traceContextAccessor = traceContextAccessor;
-//            _propagator = propagator;
-//        }
+        public ZipkinMessageInspector(
+            string applicationName,
+            IReporter reporter,
+            ITraceAccessor traceAccessor,
+            IPropagator<HttpRequestMessageProperty> propagator)
+        {
+            _applicationName = applicationName;
+            _reporter = reporter;
+            _traceAccessor = traceAccessor;
+            _propagator = propagator;
+        }
 
-//        /// <inheritdoc />
-//        public object BeforeSendRequest(ref Message request, IClientChannel channel)
-//        {
-//            var traceContext = _traceContextAccessor.Context
-//                .NewChildTrace()
-//                .Sample(_sampler);
+        public object BeforeSendRequest(ref Message request, IClientChannel channel)
+        {
+            var trace = _traceAccessor.HasTrace()
+                ? _traceAccessor.GetTrace().Refresh()
+                : new Trace();
 
-//            var httpRequest = ExtractHttpRequest(request);
+            var httpRequest = ExtractHttpRequest(request);
 
-//            // Inject X-B3 headers to the outgoing request
-//            _propagator.Inject(httpRequest, traceContext);
+            _spanBuilder = trace
+                .GetSpanBuilder()
+                .Tag("action", request.Headers.Action)
+                .WithRemoteEndpoint(new Endpoint
+                {
+                    ServiceName = _applicationName
+                })
+                .Start();
 
-//            _trace = new ClientTrace(
-//                traceContext,
-//                "soap",
-//                remote: new Endpoint
-//                {
-//                    ServiceName = _applicationName
-//                });
+            // Inject X-B3 headers to the outgoing request
+            _propagator.Inject(httpRequest, _spanBuilder.Build());
 
-//            _trace.Tag("action", request.Headers.Action);
-//            _trace.Start();
+            return null;
+        }
 
-//            return null;
-//        }
+        public void AfterReceiveReply(ref Message reply, object correlationState)
+        {
+            _spanBuilder.End();
+            _reporter.Report(_spanBuilder.Build());
+        }
 
-//        /// <inheritdoc />
-//        public void AfterReceiveReply(ref Message reply, object correlationState)
-//        {
-//            _trace.End();
-//            _reporter.Report(_trace);
-//        }
+        private static HttpRequestMessageProperty ExtractHttpRequest(Message wcfMessage)
+        {
+            HttpRequestMessageProperty httpRequest;
+            if (wcfMessage.Properties.TryGetValue(HttpRequestMessageProperty.Name, out var requestObject))
+            {
+                httpRequest = requestObject as HttpRequestMessageProperty;
+            }
+            else
+            {
+                httpRequest = new HttpRequestMessageProperty();
+                wcfMessage.Properties.Add(HttpRequestMessageProperty.Name, httpRequest);
+            }
 
-//        private static HttpRequestMessageProperty ExtractHttpRequest(Message wcfMessage)
-//        {
-//            HttpRequestMessageProperty httpRequest;
-//            if (wcfMessage.Properties.TryGetValue(HttpRequestMessageProperty.Name, out var requestObject))
-//            {
-//                httpRequest = requestObject as HttpRequestMessageProperty;
-//            }
-//            else
-//            {
-//                httpRequest = new HttpRequestMessageProperty();
-//                wcfMessage.Properties.Add(HttpRequestMessageProperty.Name, httpRequest);
-//            }
-
-//            return httpRequest;
-//        }
-//    }
-//}
+            return httpRequest;
+        }
+    }
+}
