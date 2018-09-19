44eb90e67d727f5376fe3d778f3a3a8b965ce912
jack <jackholmes5194@gmail.com>
2018-09-19 19:11:17 +1200

    fix OWIN propagation

--- Zipkin.NET.OWIN/OwinContextB3Propagator.cs
+++ Zipkin.NET.OWIN/OwinContextB3Propagator.cs
@@ -1,35 +1,20 @@
-﻿//using Microsoft.Owin;
-//using Zipkin.NET.Instrumentation;
-//using Zipkin.NET.Instrumentation.Constants;
-//using Zipkin.NET.Instrumentation.Propagation;
+﻿using Microsoft.Owin;
+using Zipkin.NET.Constants;
+using Zipkin.NET.Models;
+using Zipkin.NET.Propagation;
 
-//namespace Zipkin.NET.OWIN
-//{
-//    /// <summary>
-//    /// Injects trace context into an <see cref="IOwinContext"/>.
-//    /// </summary>
-//    public class OwinContextB3Propagator : IPropagator<IOwinContext>
-//    {
-//        /// <summary>
-//        /// Adds X-B3 header values to an outgoing HTTP request.
-//        /// </summary>
-//        /// <param name="context">
-//        /// The current OWIN context.
-//        /// </param>
-//        /// <param name="traceContext">
-//        /// The <see cref="TraceContext"/> which contains trace ID context for the current trace.
-//        /// </param>
-//        /// <returns>
-//        /// The OWIN context with trace headers.
-//        /// </returns>
-//        public IOwinContext Inject(IOwinContext context, TraceContext traceContext)
-//        {
-//            context.Request.Headers.Add(B3HeaderConstants.TraceId, new[] { traceContext.TraceId });
-//            context.Request.Headers.Add(B3HeaderConstants.SpanId, new[] { traceContext.SpanId });
-//            context.Request.Headers.Add(B3HeaderConstants.ParentSpanId, new[] { traceContext.ParentSpanId });
-//            context.Request.Headers.Add(B3HeaderConstants.Sampled, new[] { traceContext.Sampled == true ? "1" : "0" });
-//            context.Request.Headers.Add(B3HeaderConstants.Flags, new[] { traceContext.Debug == true ? "1" : "0" });
-//            return context;
-//        }
-//    }
-//}
+namespace Zipkin.NET.OWIN
+{
+    public class OwinContextB3Propagator : IPropagator<IOwinContext>
+    {
+        public IOwinContext Inject(IOwinContext inject, Span span)
+        {
+            inject.Request.Headers.Add(B3HeaderConstants.TraceId, new[] { span.TraceId });
+            inject.Request.Headers.Add(B3HeaderConstants.SpanId, new[] { span.Id });
+            inject.Request.Headers.Add(B3HeaderConstants.ParentSpanId, new[] { span.ParentId });
+            //inject.Request.Headers.Add(B3HeaderConstants.Sampled, new[] { span.Sampled == true ? "1" : "0" });
+            inject.Request.Headers.Add(B3HeaderConstants.Flags, new[] { span.Debug ? "1" : "0" });
+            return inject;
+        }
+    }
+}
