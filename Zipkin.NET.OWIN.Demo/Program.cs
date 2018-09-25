b3cf0485b5988790d9a9cc53f80fb5733cd19c6d
jack <jaackholmes@gmail.com>
2018-09-25 18:12:27 +1200

    reset zipkin endpoint for demos

--- Zipkin.NET.OWIN.Demo/Program.cs
+++ Zipkin.NET.OWIN.Demo/Program.cs
@@ -10,7 +10,7 @@ namespace Zipkin.NET.OWIN.Demo
         public static void Main(string[] args)
         {
             // Register zipkin reporter
-            var sender = new HttpSender("http://localhost:8888");
+            var sender = new HttpSender("http://localhost:9411");
             var reporter = new Reporter(sender);
             TraceManager.Register(reporter);
 
