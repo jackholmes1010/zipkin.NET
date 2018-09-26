3fd604437d324f5a6d779f0aabbcccf57c24094b
jack <jackholmes5194@gmail.com>
2018-09-26 19:26:06 +1200

    fix packages, remove build warnings

--- Zipkin.NET.Middleware/TracingApplicationBuilderExtensions.cs
+++ Zipkin.NET.Middleware/TracingApplicationBuilderExtensions.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Owin.Hosting.Services;
 using Zipkin.NET.Logging;
 using Zipkin.NET.Middleware.Logging;
 using Zipkin.NET.Middleware.TraceAccessors;
