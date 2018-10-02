c4e3c89790dbff2bee88573e9012096021f9f2c8
jack <jaackholmes@gmail.com>
2018-10-02 21:33:23 +1300

    remove static dependency class

--- examples/Zipkin.NET.OWIN.Demo/Program.cs
+++ examples/Zipkin.NET.OWIN.Demo/Program.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using Microsoft.Owin.Hosting;
-using Zipkin.NET.Dispatchers;
-using Zipkin.NET.Framework;
-using Zipkin.NET.Logging;
-using Zipkin.NET.Reporters;
-using Zipkin.NET.Sampling;
-using Zipkin.NET.Senders;
 
 namespace Zipkin.NET.OWIN.Demo
 {
@@ -14,14 +7,6 @@ namespace Zipkin.NET.OWIN.Demo
     {
         public static void Main(string[] args)
         {
-            // Register zipkin reporter
-            var sender = new ZipkinHttpSender("http://localhost:9411");
-            var zipkinReporter = new ZipkinReporter(sender);            var logger = new ConsoleInstrumentationLogger();
-            var reporters = new List<IReporter> {zipkinReporter, new ConsoleReporter()};
-            var dispatcher = new AsyncActionBlockDispatcher(reporters, logger);
-
-            StaticDependencies.TryRegister<IDispatcher>(dispatcher);
-
             const string baseAddress = "http://localhost:9055/";
 
             // Start OWIN host 
--- examples/Zipkin.NET.OWIN.Demo/Startup.cs
+++ examples/Zipkin.NET.OWIN.Demo/Startup.cs
@@ -1,8 +1,12 @@
-﻿using System.Web.Http;
+﻿using System.Collections.Generic;
+using System.Web.Http;
 using Owin;
 using Zipkin.NET.Dispatchers;
 using Zipkin.NET.Framework;
+using Zipkin.NET.Logging;
+using Zipkin.NET.Reporters;
 using Zipkin.NET.Sampling;
+using Zipkin.NET.Senders;
 
 namespace Zipkin.NET.OWIN.Demo
 {
@@ -12,7 +16,6 @@ namespace Zipkin.NET.OWIN.Demo
         // parameter in the WebApp.Start method.
         public void Configuration(IAppBuilder app)
         {
-
             // Configure Web API for self-host. 
             HttpConfiguration config = new HttpConfiguration();
 
@@ -26,10 +29,16 @@ namespace Zipkin.NET.OWIN.Demo
             
             app.Use(async (ctx, next) =>
             {
+                // Register zipkin reporter
+                var sender = new ZipkinHttpSender("http://localhost:9411");
+                var zipkinReporter = new ZipkinReporter(sender); var logger = new ConsoleInstrumentationLogger();
+                var reporters = new List<IReporter> { zipkinReporter, new ConsoleReporter() };
+                var dispatcher = new AsyncActionBlockDispatcher(reporters, logger);
+
                 var middleware = new TracingMiddleware(
                     "owin-api",
-                    new CallContextTraceContextAccessor(), 
-                    StaticDependencies.Get<IDispatcher>(),
+                    new CallContextTraceContextAccessor(),
+                    dispatcher,
                     new RateSampler(1f));
 
                 await middleware.Invoke(ctx, next);
