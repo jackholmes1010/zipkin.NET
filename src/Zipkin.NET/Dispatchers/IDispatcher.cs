6caf6f8a019959370285cc785bb4f6802dc46a52
jack <jaackholmes@gmail.com>
2018-10-06 22:36:19 +1300

    add xml comments

--- src/Zipkin.NET/Dispatchers/IDispatcher.cs
+++ src/Zipkin.NET/Dispatchers/IDispatcher.cs
@@ -2,8 +2,20 @@
 
 namespace Zipkin.NET.Dispatchers
 {
+    /// <summary>
+    /// Dispatches completed spans to reporters.
+    /// </summary>
     public interface IDispatcher
     {
+        /// <summary>
+        /// Dispatches a completed span.
+        /// </summary>
+        /// <param name="span">
+        /// The completed span.
+        /// </param>
+        /// <param name="traceContext">
+        /// The current trace context.
+        /// </param>
         void Dispatch(Span span, TraceContext traceContext);
     }
 }
--- src/Zipkin.NET/Sampling/ISampler.cs
+++ src/Zipkin.NET/Sampling/ISampler.cs
@@ -1,7 +1,19 @@
 ﻿namespace Zipkin.NET.Sampling
 {
+    /// <summary>
+    /// Samplers are used to decide if a trace should be sampled.
+    /// </summary>
     public interface ISampler
     {
+        /// <summary>
+        /// Decide if a trace should be sampled.
+        /// </summary>
+        /// <param name="traceContext">
+        /// The <see cref="TraceContext"/>.
+        /// </param>
+        /// <returns>
+        /// True if the trace should be sampled.
+        /// </returns>
         bool IsSampled(TraceContext traceContext);
     }
 }
